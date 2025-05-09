f73c58923339ab769ba8dd795cc172b1a15868ec
Bohdan <ihnatenko.bogdan@gmail.com>
2025-05-09 20:54:05 +0200

    Server Init

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
+
+// Обслуживание статических файлов React (после сборки)
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+
+// Пример простого API маршрута
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from API" });
+});
+
+// Универсальный маршрут (React Router)
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "../build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
