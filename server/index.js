30c44281e7cd595f438e4074289f73f4c95874d1
Bohdan <ihnatenko.bogdan@gmail.com>
2025-05-10 12:13:39 +0200

    localhost fix and mapping

--- server/index.js
+++ server/index.js
@@ -22,5 +22,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
