40c409d40e45a2dcc1e85f6e509fd0614dbf0b3a
Bohdan <ihnatenko.bogdan@gmail.com>
2025-05-13 01:54:02 +0200

    added webhook

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // Загружаем .env файл
 
 const express = require('express');
 const path = require('path');
+const { exec } = require('child_process');
 const app = express();
 
 // Настройка статики (исправленный путь)
@@ -15,6 +16,25 @@ app.get('/', (req, res) => {
 // Маршрут API
 app.get('/api/hello', (req, res) => {
     res.json({ message: "Hi, from API!" });
+});
+
+// Маршрут Webhook для Git
+app.post('/webhook', express.json(), (req, res) => {
+    const branch = req.body.ref;
+    if (branch === 'refs/heads/master') {
+        exec('git pull && npm install && npm start', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing command: ${error.message}`);
+                return res.status(500).json({ message: "Error executing command", error: error.message });
+            }
+
+            console.log(`Результат выполнения:\n${stdout}`);
+            console.error(`Ошибки:\n${stderr}`);
+            return res.status(200).json({ message: "Command completed", output: stdout, errors: stderr });
+        });
+    } else {
+        res.status(200).json({ message: "Push not from master, skip" });
+    }
 });
 
 // Запуск сервера
